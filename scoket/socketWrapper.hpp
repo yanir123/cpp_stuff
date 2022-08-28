fd95d4492aa8176df8afcec3504b57de64e94607
Yanir Harel <yanir44321@gmail.com>
2022-08-29 02:23:26 +0300

    feat(): Add Irreps class

--- scoket/socketWrapper.hpp
+++ scoket/socketWrapper.hpp
@@ -10,33 +10,82 @@
 #include <cstring>
 #include <exception>
 #include <string>
-
+/*
+    Exception class to alert of network failure
+*/
 class SocketException : public std::exception {
     const char* what() const throw() {
         return "Socket error";
     }
 };
 
+/*
+    Class to wrap the basic linux socket interface
+*/
 class Socket {
    public:
+    /*
+    Constructor for the socket class with existing socker fd
+        @param fd int: open socket file descriptor
+    */
     Socket(int fd);
+
+    /*
+    Constructor for the socket class with empty initialization
+    */
     Socket();
+
+    /*
+    Destructor for the socket class
+    */
     ~Socket();
+
+    /*
+    Socket class function to send data
+        @param msg string - message to send over the socket
+    */
     void send(std::string msg);
+
+    /*
+    Socket function to recieve data 
+        @param len uint16_t - max length of the expected message
+        @return string - message recieved over the socket
+    */
     std::string recv(uint16_t len);
 
    protected:
     int socketFd;
 };
 
+/*
+Class to represent client side socket
+*/
 class ClientSocket : public Socket {
    public:
+    /*
+    Constructor to initialize the socket
+        @param hostname string - hostname to connect to
+        @param port uint16_t - port to connect on
+    */
     ClientSocket(std::string hostname, uint16_t port);
 };
 
+/*
+Class to represent Server side socket
+*/
 class ServerSocket : public Socket {
    public:
+    /*
+    Constructor to initialize the socket
+        @param hostname string - hostname to bind to
+        @param port uint16_t - port to bind on
+    */
     ServerSocket(std::string hostname, uint16_t port);
+
+    /*
+    Function to accept clients
+        @return Socket - new client
+    */
     Socket* acceptClient();
 };
 
